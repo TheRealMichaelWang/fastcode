6b10254d776f432304685316f185be0727fa1b74
TheRealMichaelWang <44185024+TheRealMichaelWang@users.noreply.github.com>
2021-04-30 08:14:56 -0700

    Delete src directory

--- src/operators.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifndef OPERATORS_H
-#define OPERATORS_H
-
-value* applyUniaryOp(char type, unique_reference* value);
-value* applyBinaryOp(char type, unique_reference* a, unique_reference* b);
-
-#endif // !OPERATORS_H
